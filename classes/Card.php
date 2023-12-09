0977be6c264f1d33ef2e010bee67bfa7c6a608bf
Lubomir-Hlavina <lubo.hlavina@gmail.com>
2023-12-09 14:25:31 +0100

    insertIntoUserCard added + createCard fixed

--- classes/Card.php
+++ classes/Card.php
@@ -22,6 +22,7 @@ class Card
             error_log("Chyba vo funkii getCard, získanie dát zlyhalo\n", 3, "../errors/error.log");
             echo "Typ chyby: " . $e->getMessage();
         }
+
     }
 
 
@@ -75,10 +76,9 @@ class Card
     }
 
 
-    public static function getAllCards($connection, $columns = "*")
+    public static function getUserCards($connection, $columns = "*")
     {
-        $sql = "SELECT $columns 
-                FROM card";
+        $sql = "SELECT $columns FROM user_card u JOIN card c ON c.id = u.card_id AND u.user_id=" . $_SESSION['logged_in_user_id'];
 
         $stmt = $connection->prepare($sql);
 
@@ -86,10 +86,10 @@ class Card
             if ($stmt->execute()) {
                 return $stmt->fetchAll(PDO::FETCH_ASSOC);
             } else {
-                throw new Exception("Získanie všetkých kariet zlyhalo");
+                throw new Exception("Získanie vašich kariet zlyhalo");
             }
         } catch (Exception $e) {
-            error_log("Chyba vo funkcii getAllCards\n", 3, "../errors/error.log");
+            error_log("Chyba vo funkcii getUserCards\n", 3, "../errors/error.log");
             echo "Typ chyby: " . $e->getMessage();
         }
     }
@@ -110,6 +110,7 @@ class Card
         try {
             if ($stmt->execute()) {
                 $id = $connection->lastInsertId();
+                self::insertIntoUserCard($connection, $id);
                 return $id;
             } else {
                 throw new Exception("Vytvorenie karty zlyhalo");
@@ -120,13 +121,30 @@ class Card
         }
     }
 
+    public static function insertIntoUserCard($connection, $cardId)
+    {
+
+        $sql = "INSERT INTO user_card VALUES(:1, :2)";
+        $stmt = $connection->prepare($sql);
+
+        $stmt->bindValue(":1", $_SESSION['logged_in_user_id'], PDO::PARAM_INT);
+        $stmt->bindValue(":2", $cardId, PDO::PARAM_INT);
+
+        try {
+            if ($stmt->execute()) {
+
+            } else {
+                throw new Exception("Vloženie karty zlyhalo");
+            }
+        } catch (Exception $e) {
+            error_log("Chyba vo funkcii insertIntoCardCard\n", 3, "../errors/error.log");
+            echo "Typ chyby: " . $e->getMessage();
+        }
+    }
 
     public static function getRandomCard($connection, $columns = "*")
     {
-        $sql = "SELECT $columns
-            FROM card
-            ORDER BY RAND()
-            LIMIT 1";
+        $sql = "SELECT $columns FROM user_card u JOIN card c ON c.id = u.card_id AND u.user_id=" . $_SESSION['logged_in_user_id'] . " ORDER BY RAND() LIMIT 1";
 
         $stmt = $connection->prepare($sql);
 
